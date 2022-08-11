f482568c1b65c80f3a66252c080a545f28c62606
Matheus Correia <matheusgcorreia@gmail.com>
2022-08-11 18:28:23 -0300

    fix: catch error on route

--- src/api/routes/order.ts
+++ src/api/routes/order.ts
@@ -20,8 +20,12 @@ orderRouter.post(
   validatorMiddleware, 
   async (req: Request, res: Response) => {
     const { body: order } = req
-    const orderId = await createOrder(order.productId, order.buyerId, order.amount, new ProductRepository(), new UserRepository(), new OrderRepository())
-    return res.status(201).json(orderId)
+    try {
+      const orderId = await createOrder(order.productId, order.buyerId, order.amount, new ProductRepository(), new UserRepository(), new OrderRepository())
+      return res.status(201).json(orderId)
+    } catch (error) {
+      return res.status(400).json({ error: (error as any).message })
+    }
 })
 
 export default orderRouter
